bd12a8bd26631a99852616ff97910f9b454bad1e
Alaysia Veal <alaysiaveal@gmail.com>
2022-10-26 15:44:36 -0400

    added genres

--- client/src/components/Genres.js
+++ client/src/components/Genres.js
@@ -1,7 +1,17 @@
 const Genres = () => {
   return (
     <div>
-      <h1>Genres</h1>
+      <div className="Genres">
+        <h1>Genres</h1>
+        <h2>Action</h2>
+        <h2>Romance</h2>
+        <h2>Horror</h2>
+        <h2>Comedy</h2>
+        <h2>Mature</h2>
+        <h2>Shounen ai</h2>
+        <h2>Harem</h2>
+        <h2>Thriller</h2>
+      </div>
     </div>
   )
 }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,11 +6,11 @@ const Header = () => {
         <NavLink to="/" className="home">
           Home
         </NavLink>
-        <br></br>
+
         <NavLink to="/genres" className="genre">
           Genres
         </NavLink>
-        <br></br>
+
         <NavLink to="/list" className="list">
           List
         </NavLink>
